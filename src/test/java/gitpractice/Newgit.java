e45bba2b0337c78adf97bdbbdf7f840db2e0a87a
User <User@DESKTOP-P309ICG>
2022-01-26 22:48:54 +0530

    first commit by user1

---
+++ src/test/java/gitpractice/Newgit.java
@@ -0,0 +1,14 @@
+package gitpractice;
+
+import org.testng.annotations.Test;
+
+public class Newgit {
+	@Test
+	
+	public void newgitTest() {
+		
+		System.out.println("user1 first commit");
+		
+	}
+
+}
